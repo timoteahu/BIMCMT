2b3c795490470105bbe65e781b7598018bdcaa54
timoteahu <51691041+timoteahu@users.noreply.github.com>
2022-04-10 14:17:25 -0700

    stuff

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -1,2 +1,8 @@
 import os
 
+from flask import Flask
+from flask_mail import Mail
+
+def init_app():
+    app = Flask(__name__)
+    return app
--- main.py
+++ main.py
@@ -1,2 +1,10 @@
 import os 
 from waitress import serve
+from app.routes import init_app
+
+app = init_app()
+
+app.secret_key = "1234235534534"
+
+if __name__ == "__main__":
+    serve(app=app, host="0.0.0.0", port=8080)
